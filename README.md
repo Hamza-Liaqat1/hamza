4274c2344926c1be642998758b227091f86721b9
Hamza <hamzaliaqat1481@gmail.com>
2024-01-18 05:24:02 +0500

    updated comands

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# hamza
-🚀 Explore the World of Hamza with a Command! 🌟 Dive into Hamza's universe through your terminal. Just run `npx hamza` and embark on an interactive journey. Perfect for curious coders and creative minds alike. Let's make coding fun again! 🌐💻
+# hamza liaqat
+🚀 Explore the World of Hamza with a Command! 🌟 Dive into Hamza's universe through your terminal. Just run `npx hamzaliaqat` and embark on an interactive journey. Perfect for curious coders and creative minds alike. Let's make coding fun again! 🌐💻
---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+console.log(`
+██╗  ██╗ █████╗ ███╗   ███╗ :::::::::    █████╗               
+██║  ██║██╔══██╗████╗ ████║       :+:   ██╔══██╗               
+███████║███████║██╔████╔██║      +:+    ███████║             
+██╔══██║██╔══██║██║╚██╔╝██║     +#+     ██╔══██║               
+██║  ██║██║  ██║██║ ╚═╝ ██║    +#+      ██║  ██║               
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝  #+#        ╚═╝  ╚═╝               
+                            #########     
+                                                       
+
+🚀 Hello! I'm Hamza Liaqat, an innovative Full-Stack Developer with 5 years of 
+expertise in the MERN stack and hybrid mobile apps. Celebrated as a top-rated 
+freelancer, I specialize in transforming complex ideas into seamless digital 
+experiences. I'm passionate about delivering scalable, intuitive web and mobile 
+solutions that not only meet but exceed user expectations and drive business 
+success.
+                         
+🐦 Twitter: https://twitter.com/Hamzaliaqat_1
+🐱 GitHub:  https://github.com/Hamza-Liaqat1
+🌐 Blog:    https://www.hamzaliaqat.com
+`);
